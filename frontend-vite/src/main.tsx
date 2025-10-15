78c95e4618a2ef7f4c8eeb13b0349579a656fce5
chaehoon <ch05108@naver.com>
2025-10-15 16:24:18 +0900

    newfirst

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './routes'
 
-const router = createBrowserRouter(routes, { basename: '/student-ai-web' })
+const router = createBrowserRouter(routes, { basename: '/student' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
